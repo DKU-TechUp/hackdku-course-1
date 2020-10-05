e85620f0f9ea332b79511e42ba744b77ad3eeb11
Nick Silva <silva.d.nicholas@gmail.com>
2020-10-05 00:28:15 -0400

    update main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function List() {
+class List extends React.Component {
+  render() {
     return (
       <div className="list">
         <h1>List for {this.props.name}</h1>
@@ -12,6 +13,36 @@ function List() {
         </ul>
       </div>
     );
+  }
+}
+
+class Block extends React.Component {
+  constructor(props) {
+    this.state = { color: "#EADAAC", selected: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.state.selected = !this.state.selected;
+    this.setState((state) => ({
+      color: this.state.selected ? `#D6B85C` : "#EADAAC",
+    }));
+    this.props.callback(this.state.selected);
+  }
+
+  render() {
+    const style = {
+      backgroundColor: this.state.color,
+    };
+
+    return (
+      <div className="block" style={style}>
+        <text className="noselect" onClick={this.handleClick}>
+          {this.props.value}
+        </text>
+      </div>
+    );
+  }
 }
 
 export default List;
